27966c9315096485cf67a7868102570d0055201a
nostalfinals <yuu8583@proton.me>
2024-06-11 18:13:49 +0800

    feat(core): more extensions for Audience

--- core/src/main/kotlin/ink/pmc/advkt/AudienceKt.kt
+++ core/src/main/kotlin/ink/pmc/advkt/AudienceKt.kt
@@ -5,3 +5,11 @@ import net.kyori.adventure.audience.Audience
 fun Audience.send(message: RootComponentKt.() -> Unit) {
     this.sendMessage(RootComponentKt().apply(message).build())
 }
+
+fun Audience.showTitle(title: ComponentTitleKt.() -> Unit) {
+    this.showTitle(ComponentTitleKt().apply(title).build())
+}
+
+fun Audience.sendActionBar(actionbar: RootComponentKt.() -> Unit) {
+    this.sendActionBar(RootComponentKt().apply(actionbar).build())
+}
--- core/src/main/kotlin/ink/pmc/advkt/HoverEventKt.kt
+++ core/src/main/kotlin/ink/pmc/advkt/HoverEventKt.kt
@@ -3,8 +3,6 @@ package ink.pmc.advkt
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.event.HoverEvent
 
-
-
 fun showText(text: RootComponentKt.() -> Unit): HoverEvent<Component> {
     return HoverEvent.showText(RootComponentKt().apply(text).build())
 }
