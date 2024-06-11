89aa19096848c939cfcfc0dc8b5546990f922d45
DeeChael <DeeChael@outlook.com>
2024-06-11 15:07:33 +0800

    feat: book support

---
+++ core/src/main/kotlin/ink/nostal/advkt/BookKt.kt
@@ -0,0 +1,32 @@
+package ink.nostal.advkt
+
+import net.kyori.adventure.inventory.Book
+import net.kyori.adventure.text.Component
+
+class BookKt {
+
+    internal var title: Component = Component.empty()
+    internal var author: Component = Component.empty()
+    internal val pages = mutableListOf<Component>()
+
+    fun build(): Book {
+        return Book.book(title, author, pages)
+    }
+
+}
+
+fun book(content: BookKt.() -> Unit): Book {
+    return BookKt().apply(content).build()
+}
+
+fun BookKt.title(content: RootComponentKt.() -> Unit) {
+    this.title = RootComponentKt().apply(content).build()
+}
+
+fun BookKt.author(content: RootComponentKt.() -> Unit) {
+    this.author = RootComponentKt().apply(content).build()
+}
+
+fun BookKt.page(content: RootComponentKt.() -> Unit) {
+    this.pages.add(RootComponentKt().apply(content).build())
+}
---
+++ test/src/main/kotlin/ink/nostal/advkt/BookKtTest.kt
@@ -0,0 +1,19 @@
+package ink.nostal.advkt
+
+fun main() {
+    val book = book {
+        title {
+            text("Just a book")
+        }
+        author {
+            text("DeeChael")
+        }
+
+        page {
+            text("You are in the first page")
+        }
+        page {
+            text("now in the second!")
+        }
+    }
+}
